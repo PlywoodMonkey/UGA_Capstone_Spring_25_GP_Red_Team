c961b6484e79d86f11570cd454fab251d4f82a44
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2025-02-18 14:50:29 -0500

    Create RF_Transmitter.ino

---
+++ Arduino_Code/RF_Transmitter.ino
@@ -0,0 +1,30 @@
+/*
+* RF_Transmitter.ino
+* Adapted from Code by Dejan Nedelkvoski, www.HowToMechatronics.com
+*
+* Adapted for use in UGA's MCHE 4911 Capstone. 
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+
+void loop() {
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  delay(1000);
+}
+
