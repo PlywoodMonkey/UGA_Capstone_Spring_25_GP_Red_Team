1f56bd9f6120b75c5f9bda9ba75995f2f9d61956
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2025-03-26 12:30:10 -0400

    Three Working LEDs fed by GPIO Outputs
    Currently turns on only one LED at a time, either the Green, Red, or Yellow one. Proof of concept for the handheld controller as it is controlled by output GPIO pins which will go to the battery and stepper motor.

--- Arduino_Code/HandHeldController.ino
+++ Arduino_Code/HandHeldController.ino
@@ -6,7 +6,8 @@
 *  The following code is used to control whether the claw on the drone attachment is opening or closing. Two LEDs are 
 *  used to signify the correct output of voltage to the motor.
 *
-*  Simply renamed the variables and resetup the LEDs in order to provide a way to output to them instead of connecting 3.3V power.
+*  Currently turns on only one LED at a time, either the Green, Red, or Yellow one. Proof of concept for the handheld controller
+*  as it is controlled by output GPIO pins which will go to the battery and stepper motor. 
 */
 
 // Create variables for ease of changing pins
@@ -17,25 +18,67 @@ int greenLEDInput   = 5; // Orange Dupont
 int redLEDInput     = 6; // Purple Dupont
 
 
+// Code for the setup
 void setup() {
 
   Serial.begin(9600);       // Setup the Serial Output
 
-  pinMode(greenLEDInput, INPUT); // Setup the current GPIO pin for the Green LED as an input
-  pinMode(redLEDInput, INPUT);   // Setup the current GPIO pin for the Red LED as an input
+  pinMode(greenLEDOutput,   OUTPUT); // Setup the current GPIO pin for the Green LED as an output
+  pinMode(yellowLEDOutput,  OUTPUT); // Setup the current GPIO pin for the Yellow LED as an output
+  pinMode(redLEDOutput,     OUTPUT); // Setup the current GPIO pin for the Red LED as an output
+  pinMode(greenLEDInput,    INPUT);  // Setup the current GPIO pin for the left button as an input
+  pinMode(redLEDInput,      INPUT);  // Setup the current GPIO pin for the right button as an input
 
   Serial.println("start");  // Communicate to user that program has begun
 } // end of setup
 
 
+// Main loop to be run continously
 void loop() {
 
   if ((digitalRead(greenLEDInput) && digitalRead(redLEDInput)) == 1) {
     Serial.println("Both buttons are pushed: Do Nothing");
+    turnOnYellowLED();
   } else if (digitalRead(greenLEDInput) == 1) {
-    Serial.println("The Green LED is pushed");
+    Serial.println("The Green LED is pushed");    
+    turnOnGreenLED();
   } else if (digitalRead(redLEDInput) == 1) {
     Serial.println("The Red LED is pushed");
-  } // end of if/else-if
+    turnOnRedLED();
+  } else {
+    turnOffAllLEDs();  
+  }// end of if/else-if/else
 
 } // end of loop
+
+
+// Turns off all of the LEDs at once
+void turnOffAllLEDs() {
+  digitalWrite(greenLEDOutput,  0);
+  digitalWrite(yellowLEDOutput, 0);
+  digitalWrite(redLEDOutput,    0);
+}
+
+
+// Turns on ONLY the Green LED
+void turnOnGreenLED(){
+  digitalWrite(greenLEDOutput,  1);
+  digitalWrite(yellowLEDOutput, 0);
+  digitalWrite(redLEDOutput,    0);
+}
+
+
+// Turns on ONLY the Yellow LED
+void turnOnYellowLED(){
+  digitalWrite(greenLEDOutput,  0);
+  digitalWrite(yellowLEDOutput, 1);
+  digitalWrite(redLEDOutput,    0);
+}
+
+
+// Turns on ONLY the Red LED
+void turnOnRedLED(){
+  digitalWrite(greenLEDOutput,  0);
+  digitalWrite(yellowLEDOutput, 0);
+  digitalWrite(redLEDOutput,    1);
+}
