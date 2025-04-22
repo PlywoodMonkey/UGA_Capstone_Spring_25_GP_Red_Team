3fcef934660f60b42f272192db88d5f36cebaaf2
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2025-04-22 17:12:14 -0400

    Working Cla

--- Arduino_Code/ClawController.ino
+++ Arduino_Code/ClawController.ino
@@ -1,35 +1,61 @@
-/* 
-*  File   - ClawController.ino
-*  Author - Samuel Brewster
-*  Class  - MCHE 4911
-*
-*  Description to be written.
-*/
+#include <Servo.h>
 
-int closeArmInput  = 7;
-int openArmInput   = 6;
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+int greenLEDOutput  = 2; // Green Dupont
+int yellowLEDOutput = 3; // Yellow Dupont
+int redLEDOutput    = 4; // Red Dupont
+int greenLEDInput   = 5; // Orange Dupont
+int redLEDInput     = 6; // Purple Dupont
+
+double degreesRotated  = 90;
 
 void setup() {
-  // put your setup code here, to run once:
-
-  Serial.begin(9600);       // Setup the Serial Output
-
-  pinMode(closeArmInput,     INPUT);  // Setup the current GPIO pin for the left button as an input
-  pinMode(openArmInput,      INPUT);  // Setup the current GPIO pin for the right button as an input
-
-  Serial.println("start");  // Communicate to user that program has begun
-
+  Serial.begin(9600);
+  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  if ((digitalRead(openArmInput)) == 1) {
-    // Put PWM Open here
-    Serial.println("Opening");
-  } else if (digitalRead(closeArmInput) == 1) {
-    // Put PWM Close here
-    Serial.println("Closing");
-  } 
+  if ((digitalRead(greenLEDInput) && digitalRead(redLEDInput)) == 1) {
 
-}
+    Serial.print(degreesRotated);
+    Serial.println(" : Claw is not moving (BOTH BUTTONS PUSHED)");
+
+  } else if (digitalRead(greenLEDInput) == 1) {
+    
+    if (degreesRotated < 200){
+      degreesRotated += 2;
+      Serial.print(degreesRotated);
+      Serial.println(" : Opening Claw (GREEN LED ON)");    
+    } else if (degreesRotated == 90) {
+      Serial.println("Claw is Fully Opened");
+    }
+
+  } else if (digitalRead(redLEDInput) == 1) {
+
+    if (degreesRotated > 0){
+      degreesRotated -= 2;
+      Serial.print(degreesRotated);
+      Serial.println(" : Closing Claw (RED LED ON)");
+    } else if (degreesRotated == 0) {
+      Serial.println("Claw is Fully Closed");
+    }
+
+  } else if (degreesRotated == 0) {
+    
+    Serial.println("Claw is Fully Closed");
+
+  } else if (degreesRotated == 360) {
+
+    Serial.println("Claw is Fully Opened");
+
+  } else {
+  }// end of if/else-if/else
+
+  myservo.write(degreesRotated);  
+
+} // end of loop
