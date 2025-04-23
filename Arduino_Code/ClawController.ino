dc956e476bffb971a79ff5817bb2725cf92bd5d8
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2025-04-23 09:21:43 -0400

    Ready for Showcase

--- Arduino_Code/ClawController.ino
+++ Arduino_Code/ClawController.ino
@@ -3,58 +3,39 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
+int closeArmInput   = 2;  // Green Dupont
+int openArmInput    = 3;  // Yellow Dupont
+int outputToMotor   = 10; // Red Dupont
 
-int greenLEDOutput  = 2; // Green Dupont
-int yellowLEDOutput = 3; // Yellow Dupont
-int redLEDOutput    = 4; // Red Dupont
-int greenLEDInput   = 5; // Orange Dupont
-int redLEDInput     = 6; // Purple Dupont
-
-double degreesRotated  = 90;
+double degreesRotated  = 0;
 
 void setup() {
+
   Serial.begin(9600);
-  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(outputToMotor);  // attaches the servo on pin 10 to the servo object
+
 }
 
 void loop() {
 
-  if ((digitalRead(greenLEDInput) && digitalRead(redLEDInput)) == 1) {
+  if ((digitalRead(closeArmInput) && digitalRead(openArmInput)) == 1) {
 
     Serial.print(degreesRotated);
     Serial.println(" : Claw is not moving (BOTH BUTTONS PUSHED)");
 
-  } else if (digitalRead(greenLEDInput) == 1) {
+  } else if (digitalRead(openArmInput) == 1) {
     
-    if (degreesRotated < 200){
       degreesRotated += 2;
       Serial.print(degreesRotated);
       Serial.println(" : Opening Claw (GREEN LED ON)");    
-    } else if (degreesRotated == 90) {
-      Serial.println("Claw is Fully Opened");
-    }
 
-  } else if (digitalRead(redLEDInput) == 1) {
+  } else if (digitalRead(closeArmInput) == 1) {
 
-    if (degreesRotated > 0){
       degreesRotated -= 2;
       Serial.print(degreesRotated);
       Serial.println(" : Closing Claw (RED LED ON)");
-    } else if (degreesRotated == 0) {
-      Serial.println("Claw is Fully Closed");
-    }
 
-  } else if (degreesRotated == 0) {
-    
-    Serial.println("Claw is Fully Closed");
-
-  } else if (degreesRotated == 360) {
-
-    Serial.println("Claw is Fully Opened");
-
-  } else {
-  }// end of if/else-if/else
+  }
 
   myservo.write(degreesRotated);  
 
